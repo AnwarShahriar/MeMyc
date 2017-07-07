11a78a0f63ed3d05e103124edb75f9474aebb619
anwarshahriar <anwarshahriar@gmail.com>
2017-07-07 23:23:11 +0600

    Added barebone structure for displa

--- memyc-desktop/index.js
+++ memyc-desktop/index.js
@@ -17,11 +17,11 @@ function onClosed() {
 
 function createMainWindow() {
 	const win = new electron.BrowserWindow({
-		width: 600,
-		height: 400
+		width: 480,
+		height: 854
 	});
 
-	win.loadURL(`file://${__dirname}/index.html`);
+	win.loadURL(`file://${__dirname}/display/display.html`);
 	win.on('closed', onClosed);
 
 	return win;
