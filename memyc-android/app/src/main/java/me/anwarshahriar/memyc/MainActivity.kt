767303a2c3351423cc297d45923f1ae847195a36
anwarshahriar <anwarshahriar@gmail.com>
2017-08-03 22:23:40 +0600

    Virtual display added

--- memyc-android/app/src/main/java/me/anwarshahriar/memyc/MainActivity.kt
+++ memyc-android/app/src/main/java/me/anwarshahriar/memyc/MainActivity.kt
@@ -1,18 +1,30 @@
 package me.anwarshahriar.memyc
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
+import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Display
+import android.view.Surface
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-  var screenCaptureIntent: Intent? = null
+  private val REQUEST_MEDIA_PROJECTION = 1
 
-  val requestCaptureButton: Lazy<Button> = lazy { findViewById<Button>(R.id.capture_request_button) }
+  private val requestCaptureButton: Lazy<Button> = lazy { findViewById<Button>(R.id.capture_request_button) }
+
+  private lateinit var mediaProjection: MediaProjection
+  private lateinit var surface: Surface
+  private lateinit var handler: Handler
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -22,14 +34,43 @@ class MainActivity : AppCompatActivity() {
   }
 
   private fun init() {
-    prepareCaptureIntent()
     requestCaptureButton.value.setOnClickListener {
-      Toast.makeText(this, "Hello Memyc", Toast.LENGTH_LONG).show()
+      val captureIntent = prepareCaptureIntent();
+      startActivityForResult(captureIntent, REQUEST_MEDIA_PROJECTION)
     }
   }
 
-  private fun prepareCaptureIntent() {
-    val projectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-    screenCaptureIntent = projectionManager.createScreenCaptureIntent()
+  private fun prepareCaptureIntent(): Intent {
+    return getProjectionManager().createScreenCaptureIntent()
+  }
+
+  private fun getProjectionManager(): MediaProjectionManager {
+    return getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_MEDIA_PROJECTION) {
+      mediaProjection = getProjectionManager().getMediaProjection(resultCode, data)
+      createProjectionHandler()
+      createVirtualDisplay(mediaProjection)
+    }
+  }
+
+  private fun createVirtualDisplay(mediaProjection: MediaProjection): VirtualDisplay {
+    val displayMetric = resources.displayMetrics
+    return mediaProjection.createVirtualDisplay("MeMyc",
+        320,
+        480,
+        displayMetric.densityDpi,
+        DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+        null,
+        null,
+        handler)
+  }
+
+  private fun createProjectionHandler() {
+    val projectionThread = HandlerThread("Projection Thread");
+    projectionThread.start()
+    handler = Handler(projectionThread.looper)
   }
 }
